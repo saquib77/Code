61e5d5f4416fb7d538eb4bc3ed7c92d2a165d4b8
saquib77 <chand567khan@gmail.com>
2021-02-28 07:38:05 +0530

    Java Notes

---
+++ hsCode.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int d,i,s,v,f;
+	cin>>d>>i>>s>>v>>f;
+	for(int i=0;i<s;i++){
+		int b,e;
+		cin>>b>>e;
+		string s;
+		cin>>s;
+		
+	}
+}
